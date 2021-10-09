1340dc891e4bfe03eaea28c6ffd5ddd9f2716af1
unknown <mahmudsazzad85@gmail.com>
2021-10-09 14:55:42 +0600

    commit

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCXlFYnNTrjz6p-3VpIl7O_sOjnqn6A6g0",
+    authDomain: "simple-firebase-authenti-1b67d.firebaseapp.com",
+    projectId: "simple-firebase-authenti-1b67d",
+    storageBucket: "simple-firebase-authenti-1b67d.appspot.com",
+    messagingSenderId: "30577371453",
+    appId: "1:30577371453:web:5b99e39818e5d49b66fd54"
+};
+
+export default firebaseConfig;
---
+++ src/components/PlaceOrder/PlaceOrder.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PlaceOrder = () => {
+    return (
+        <div>
+            <h2>This is Place Order.</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sed rem tempora vel eveniet, commodi adipisci temporibus ad odio neque qui voluptatem minima architecto amet recusandae dignissimos error officia eius animi deleniti nesciunt enim facilis provident! Consequuntur, iste deleniti dolores doloribus voluptas cum modi, quod minus, ullam praesentium laboriosam nam atque possimus at. Perferendis odit porro provident totam exercitationem cupiditate commodi illo aperiam animi enim corporis cumque cum voluptas qui corrupti minima quo quidem, odio sapiente? Non laboriosam magni quo reiciendis eveniet autem! Odio reprehenderit iure odit. Expedita ducimus consequuntur, provident illum deleniti minus nesciunt ratione consequatur adipisci, similique sit.</p>
+        </div>
+    );
+};
+
+export default PlaceOrder;
