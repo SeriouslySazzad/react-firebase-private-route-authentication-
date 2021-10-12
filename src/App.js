24f1a06ea78783fa16b03c6cdc677a937783e96d
unknown <mahmudsazzad85@gmail.com>
2021-10-12 18:58:22 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,49 +1,11 @@
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './components/Home/Home';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-import Header from './components/Header/Header';
-import AuthProvider from './Context/AuthProvider';
-import Shipping from './components/Shipping/Shipping';
-import PrivateRoute from './components/PrivateRoute/PrivateRoute';
-import PlaceOrder from './components/PlaceOrder/PlaceOrder';
+import Home from './Pages/Home/Home/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <AuthProvider>
-        <BrowserRouter>
-          <Header></Header>
-          <Switch>
-
-            <Route exact path="/">
-              <Home></Home>
-            </Route>
-
-            <Route path="/home">
-              <Home></Home>
-            </Route>
-
-            <Route path="/login">
-              <Login></Login>
-            </Route>
-
-            <Route path="/register">
-              <Register></Register>
-            </Route>
-
-            <PrivateRoute path="/shipping">
-              <Shipping></Shipping>
-            </PrivateRoute>
-
-            <PrivateRoute path="/placeorder">
-              <PlaceOrder></PlaceOrder>
-            </PrivateRoute>
-
-          </Switch>
-        </BrowserRouter>
-      </AuthProvider>
+      <Home></Home>
     </div>
   );
 }
